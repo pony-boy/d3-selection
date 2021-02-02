473b3ba49469b41d782a71abe0a1e798683f5d7a
Philippe Rivière <fil@rezo.net>
2021-02-02 11:20:07 +0100

    remove all children
    fixes #275

--- test/selection/remove-test.js
+++ test/selection/remove-test.js
@@ -35,3 +35,13 @@ tape("selection.remove() skips missing elements", function(test) {
   test.equal(two.parentNode, document.body);
   test.end();
 });
+
+tape("selectChildren().remove() removes all children", function(test) {
+  var document = jsdom("<div><span>0</span><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span><span>6</span><span>7</span><span>8</span><span>9</span></div>"),
+      p = document.querySelector("div"),
+      selection = d3.select(p).selectChildren();
+  test.equal(selection.size(), 10);
+  test.equal(selection.remove(), selection);
+  test.equal(d3.select(p).selectChildren().size(), 0);
+  test.end();
+});
